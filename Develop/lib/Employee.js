cb8b781d3133b182bfc077bf0b16cd1d5639c328
Dani JM <xdanielmtz@gmail.com>
2020-11-08 00:27:09 -0500

    tests for employee are working. created the employee class and its properties and methods.

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,33 @@
 // TODO: Write code to define and export the Employee class
+
+class Employee {
+    constructor(name, id, email) {
+        this.name = name;
+        this.id = id;
+        this.email = email;
+    };
+    
+    //returns the name
+    getName() {
+        return this.name;
+      };
+
+    //returns the id
+    getId() {
+        return this.id;
+      };
+      
+      //returns the email
+    getEmail() {
+        return this.email;
+      };
+
+      //returns employee
+      getRole() {
+        return "Employee";
+      };
+
+
+}
+
+module.exports = Employee;
