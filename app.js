f7a9abbe52d90a94b7ef5aaed16c13d3fc1de9de
Dani JM <xdanielmtz@gmail.com>
2020-11-08 22:06:07 -0500

    added more questions for the user about each of their possible roles

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
+// const path = require("path");
 const fs = require("fs");
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
-const render = require("./lib/htmlRenderer");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const render = require("./lib/htmlRenderer");
 
 function promptUser(){
 
@@ -37,7 +37,7 @@ function promptUser(){
                 },
                 {
                     type: "input",
-                    message: "what is your school?",
+                    message: "What is your school?",
                     name: "school"
                 }
             ])
@@ -46,8 +46,38 @@ function promptUser(){
             inquirer.prompt([
                 {
                     type: "input",
-                    message: "What is your name?"
+                    message: "What is your ID?",
+                    name: "id" 
                 },
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "email",
+                },
+                {
+                    type: "input",
+                    message: "What is your Github username?",
+                    name: "github"
+                }
+            ])
+        }
+        if (response.role === "Manager"){
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your ID?",
+                    name: "id"
+                },
+                {
+                    type: "input",
+                    message: "What is your email?",
+                    name: "email"
+                },
+                {
+                    type: "input",
+                    message: "What is your office number?",
+                    name: "officeNumber"
+                }
             ])
         }
     })
