bbec967e51dd43c3415bb363ad254b968dd48307
Dani JM <xdanielmtz@gmail.com>
2020-11-08 21:33:55 -0500

    added series of questions based if user is an intern

--- app.js
+++ app.js
@@ -1,47 +1,61 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
-const path = require("path");
-const fs = require("fs");
+// const path = require("path");
+// const fs = require("fs");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const render = require("./lib/htmlRenderer");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output");
-const outputPath = path.join(OUTPUT_DIR, "team.html");
+function promptUser(){
 
-const render = require("./lib/htmlRenderer");
-
-function promptTheUser(){
-    inquirer.prompt([
+    inquirer.prompt([ 
         {
-            type : "list",
+            type : "input",
+            message: "What is your name?",
+            name: "name",
+        },
+        {
+            type: "list",
             message: "What is your role in your team?",
             name: "role",
             choices: ["Intern", "Engineer", "Manager"]
         }
-    ])
+    ]).then(response => {
+        if (response.role === "Intern"){
+            inquirer.prompt([ 
+                {
+                    type : "input",
+                    message: "What is your ID?",
+                    name: "id",
+                },
+                {
+                    type: "input",
+                    message: "What is your email?", 
+                    name: "email"
+                },
+                {
+                    type: "input",
+                    message: "what is your school?",
+                    name: "school"
+                }
+            ])
+        }
+        if(response.role === "Engineer"){
+            inquirer.prompt([
+                {
+                    type: "input",
+                    message: "What is your name?"
+                },
+            ])
+        }
+    })
+
+
 }
 
-promptTheUser();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+promptUser();
 
 
 
