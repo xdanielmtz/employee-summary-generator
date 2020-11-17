e9e914573ac1ad280f6c7ef2de0fa436e60a6403
Dani JM <xdanielmtz@gmail.com>
2020-11-17 15:54:12 -0500

    added final touches to make app function.

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 const render = require("./Develop/lib/htmlRenderer");
 const { removeAllListeners } = require("process");
-
+var responseInputName = '';
+const theTeam = [];
 // Function that starts app.
 startPrompts();
 
@@ -29,12 +30,15 @@ function startPrompts(){
     ]).then(response => {
         // Depending on the user response, they will be presented with a different series of prompts. 
         if (response.role === "Intern"){
-          return userIntern();
+            responseInputName=response.name;
+            return userIntern();
         }
         if(response.role === "Engineer"){
+            responseInputName=response.name;
             return userEngineer ();
         }
         if (response.role === "Manager"){
+            responseInputName=response.name;
             return userManager ();
         }
     })
@@ -58,7 +62,9 @@ function userIntern (){
             message: "What is your school?",
             name: "school"
         }
-    ]).then(response => {
+    ]).then(responseIntern => {
+        const newIntern = new Intern(responseInputName, responseIntern.id, responseIntern.email, responseIntern.school)
+        theTeam.push(newIntern);
         return repeat();
     })
 };
@@ -80,7 +86,9 @@ function userEngineer () {
             message: "What is your Github username?",
             name: "github"
         }
-    ]).then(response => {
+    ]).then(responseEngineer => {
+        const newEngineer = new Engineer(responseInputName, responseEngineer.id, responseEngineer.email, responseEngineer.github)
+        theTeam.push(newEngineer);
         return repeat();
     })
 };
@@ -102,7 +110,9 @@ function userManager (){
             message: "What is your office number?",
             name: "officeNumber"
         }
-    ]).then(response => {
+    ]).then(responseManager => {
+        const newManager = new Manager(responseInputName, responseManager.id, responseManager.email, responseManager.officeNumber)
+        theTeam.push(newManager)
         return repeat();
     })
 };
@@ -118,127 +128,14 @@ function repeat(){
         }
     ]).then(response => {
         if(response.data === true){
-            return teamData();
+            return startPrompts();
         }
         else if(response.data === false){
             console.log("Summary created!");
+            fs.writeFile(outputPath, render(theTeam), "utf-8", err => {
+                if (err) throw err
+
+            })
         }
     })
 };
-
-
-function teamData (){
-inquirer.prompt([
-    {
-        type: "list",
-        message: "What is their role?",
-        name: "teamRole",
-        choices: ["Intern", "Engineer", "Manager"]
-    },
-]).then(response => {
-    if(response.teamRole === "Intern"){
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is their ID?",
-                name:"id"
-            },
-            {
-                type: "input",
-                message: "What is their email?",
-                name:"email"
-            },
-            {
-                type: "input",
-                message: "What is their school?",
-                name:"school"
-            },
-        ]).then(response => {
-            repeat();
-        })
-    }
-    if(response.teamRole === "Engineer"){
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is their ID?",
-                name:"id"
-            },
-            {
-                type: "input",
-                message: "What is their email?",
-                name:"email"
-            },
-            {
-                type: "input",
-                message: "What is their Github username?",
-                name:"github"
-            },
-        ]).then(response => {
-            repeat();
-        })
-    }
-    if (response.teamRole === "Manager"){
-        inquirer.prompt([
-            {
-                type: "input",
-                message: "What is their ID?",
-                name:"id"
-            },
-            {
-                type: "input",
-                message: "What is their email?",
-                name:"email"
-            },
-            {
-                type: "input",
-                message: "What is their office number?",
-                name:"officeNumber"
-            },
-        ]).then(response => {
-            repeat();
-        })
-    }
-})
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
